13c8def6862a4367d0568d8a52e1b9b210cd5e94
cosmoglint <ramprakashsmail@gmail.com>
2020-12-30 20:46:16 +0530

    EDIT: remove unwanted content and fix range to fit the four million mark (although this doesnt make code efficient

--- problem_002.py
+++ problem_002.py
@@ -5,15 +5,13 @@
 # phi = 1.618034
 # golden ration fibonacci formula of (nth number ) = ((phi ** n) - ((1 - phi) ** n))  // (5**(1/2))
 
-# val = 0
 phi = 1.618034
 sum = 0
-for i in range(3,101,3):
-    # print((i*4)+val)
+for i in range(3,37,3):
     val = int(((phi ** i) - ((1 - phi) ** i))  // (5**(1/2)))
     if (val>4000000):
+        print(i)
         break
     sum += val;
-    # val = i
 
 print(sum)
