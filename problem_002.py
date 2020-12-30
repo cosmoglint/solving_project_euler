0730f46404ce0537a9627b5b4fc71097d55cb479
cosmoglint <ramprakashsmail@gmail.com>
2020-12-30 20:43:24 +0530

    STOP:  finish problem two with triple jumps and golden ratio formula

--- problem_002.py
+++ problem_002.py
@@ -5,9 +5,15 @@
 # phi = 1.618034
 # golden ration fibonacci formula of (nth number ) = ((phi ** n) - ((1 - phi) ** n))  // (5**(1/2))
 
-val = 0
+# val = 0
 phi = 1.618034
+sum = 0
 for i in range(3,101,3):
     # print((i*4)+val)
-    print(int(((phi ** i) - ((1 - phi) ** i))  // (5**(1/2))));
+    val = int(((phi ** i) - ((1 - phi) ** i))  // (5**(1/2)))
+    if (val>4000000):
+        break
+    sum += val;
     # val = i
+
+print(sum)
