04ef27aa0cf3d647632c2c9c3ceaf1d1ba720a1e
cosmoglint <ramprakashsmail@gmail.com>
2022-01-25 23:16:21 +0530

    ADD:  proper answer with elixir

--- elixir/001.exs
+++ elixir/001.exs
@@ -1,12 +1,12 @@
+# create a stream with the required range, filter and add it as you please
 
-
-mult_n? = &(rem(&1, &2) != 0)
-
-check_rem = fn divident -> ( fn value -> rem(value, divident) != 0 end ) end
+check_rem = fn divident -> ( fn value -> rem(value, divident) == 0 end ) end
 
 div_3? = check_rem.(3)
 div_5? = check_rem.(5)
 
+check_3_5? = fn value -> (div_3?.(value) || div_5?.(value)) end
+
 sum_num = &(&1 + &2)
 
-test = 1..1000 |> Stream.filter(div_5?) |> Stream.filter(div_3?) |> Enum.reduce(0, sum_num) |> IO.inspect()
+ 1..999 |> Stream.filter(check_3_5?) |> Enum.reduce(0, sum_num) |> IO.inspect()
