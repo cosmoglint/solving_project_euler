78e53765bf71ed00ba8912b002b36623aaa4836f
cosmoglint <ramprakashsmail@gmail.com>
2022-01-29 15:01:44 +0530

    working  eratosthenes sieve

--- elixir/003.exs
+++ elixir/003.exs
@@ -10,13 +10,14 @@ defmodule Prime do
   def eradicate([head | tail]) when tail != []do
     div_head? =  check_rem(head)
     newtail = tail
-      |> Enum.drop_every(head)
+      |> Enum.reject(fn x -> div_head?.(x) end)
       |> Enum.to_list
+      |> IO.inspect
     [head | eradicate(newtail)]
   end
 
   def eradicate(val) do
-    [val]
+    val
   end
 
 #  def eratosthenos(val) do
@@ -26,7 +27,7 @@ defmodule Prime do
 #  end
 end
 
-1..10 |> Enum.drop_while(fn x -> x==5 end) |> IO.inspect
+#1..10 |> Enum.drop_while(fn x -> x==5 end) |> IO.inspect
 
-#Prime.eradicate([2,3,4,5,6,7]) |> IO.inspect
+Prime.eradicate(2..20 |> Enum.to_list) |> IO.inspect
 #IO.puts Prime.largestPrimeFactor 600851475143
