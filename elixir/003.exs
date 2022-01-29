50dcce545fbd18e939104966c46f90ab9354a15f
cosmoglint <ramprakashsmail@gmail.com>
2022-01-29 11:41:01 +0530

    random  tests

--- elixir/003.exs
+++ elixir/003.exs
@@ -1,25 +1,28 @@
 defmodule Prime do
-  def eradicate(object) do
-    {iter,value} = object
-  end
+#  def eradicate(object) do
+#    {iter,value} = object
+#  end
+  #
 
-  def check_mult(n) do
+  def check_rem(n) do
     fn value -> rem(value, n) == 0 end
   end
 
-  def eradicate({head | tail}) when tail = {}do
+  def eradicate([head | tail]) when tail != {}do
+    div_head? =  check_rem(head)
     newtail = tail
-      |> Enum.drop_while(check_mult(head).(x))
-      |> IO.puts
+      |> Enum.drop_while(fn x -> div_head?.(x) end)
+    newtail
     #{head | eradicate(newtail)}
   end
 
-  def eratosthenos(val) do
-    1..val
-      |> Stream.each(fn x -> {x: true} end)
-      |> Stream.map(eradicate)
-  end
+#  def eratosthenos(val) do
+#    1..val
+#    |> Stream.each(fn x -> [x: true] end)
+#      |> Stream.map(eradicate)
+#  end
 end
 
-Prime.eradicate({1,2,3,4,5,6,7})
+
+Prime.eradicate([1,2,3,4,5,6,7]) |> IO.puts
 #IO.puts Prime.largestPrimeFactor 600851475143
