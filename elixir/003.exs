a16c715ccf8ec17d3ab086f0af13b2aaba22ef88
cosmoglint <ramprakashsmail@gmail.com>
2022-01-29 14:44:37 +0530

    testing  some stuff

--- elixir/003.exs
+++ elixir/003.exs
@@ -2,18 +2,21 @@ defmodule Prime do
 #  def eradicate(object) do
 #    {iter,value} = object
 #  end
-  #
 
   def check_rem(n) do
     fn value -> rem(value, n) == 0 end
   end
 
-  def eradicate([head | tail]) when tail != {}do
+  def eradicate([head | tail]) when tail != []do
     div_head? =  check_rem(head)
     newtail = tail
       |> Enum.drop_while(fn x -> div_head?.(x) end)
-    newtail
-    #{head | eradicate(newtail)}
+      |> Enum.to_list
+    [head | eradicate(newtail)]
+  end
+
+  def eradicate(val) do
+    [val]
   end
 
 #  def eratosthenos(val) do
@@ -23,6 +26,7 @@ defmodule Prime do
 #  end
 end
 
+1..10 |> Enum.drop_while(fn x -> x==5 end) |> IO.inspect
 
-Prime.eradicate([1,2,3,4,5,6,7]) |> IO.puts
+#Prime.eradicate([2,3,4,5,6,7]) |> IO.inspect
 #IO.puts Prime.largestPrimeFactor 600851475143
